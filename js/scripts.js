b039a3d93286b8d86fb36471053cefea63bb4d80
davidedica-coach-1 <davidedica+001@gmail.com>
2023-01-02 13:24:03 +0100

    Portfolio fix

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,9 @@ function matchHeightPortfolio() {
       //byRow: false
     //});
 
-    $('.port__proj--title').matchHeight();
+    $('.port__proj--title').matchHeight({
+      byRow: false
+    });
 }
 
 function matchHeightBlog() {
